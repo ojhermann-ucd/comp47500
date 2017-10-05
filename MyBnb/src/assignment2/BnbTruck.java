820ed2a9f9535b34bbaeca2064c6cde9729277d6
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-05 22:24:18 +0100

    classes and interface done; inputs etc in the morning

---
+++ MyBnb/src/assignment2/BnbTruck.java
@@ -0,0 +1,52 @@
+package assignment2;
+
+public class BnbTruck extends BnbVehicle {
+
+	
+	// OBJECT DATA FIELDS
+	//______________________________________________________________________
+	private int cargoWeight;
+	
+	
+	// CONSTRUCTORS
+	//______________________________________________________________________
+	// specified constructor
+	public BnbTruck(String owner, int totalRentalDays, int rentalCostPerDay, int cargoWeight) {
+		super(owner, totalRentalDays, rentalCostPerDay);
+		this.cargoWeight = cargoWeight;
+	}
+	
+	// empty constructor
+	public BnbTruck() {
+		super();
+		this.cargoWeight = 0;
+	}
+	
+	
+	// SETTER METHODS
+	//______________________________________________________________________
+	// cargoWeight
+	public void setcargoWeight(int cargoWeight) {
+		this.cargoWeight = cargoWeight;
+	}
+	
+	
+	// GETTER METHODS
+	//______________________________________________________________________
+	// cargoWeight
+	public int getcargoWeight() {
+		return this.cargoWeight;
+	}
+
+	
+	// OTHER METHODS
+	//______________________________________________________________________
+	// toString
+	@Override
+	public String toString() {
+		return
+				"Vehicle Type: Car" + "\n" +
+				super.toString() +
+				"Cargo Weight: " + String.valueOf(this.getcargoWeight()) + "\n";
+	}
+}
