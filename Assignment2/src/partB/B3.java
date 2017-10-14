53f830d355f6892af718e845ee3d965e2377d61a
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-14 20:17:59 +0100

    B3 done

---
+++ Assignment2/src/partB/B3.java
@@ -0,0 +1,46 @@
+package partB;
+
+
+//IMPORTS
+import java.util.Random;
+
+
+public class B3 {
+	
+	// MAIN
+	public static void main (String[] args) {
+		
+		// random number: rn
+		Random rn = new Random();
+		
+		// five examples of MaxInt
+		for (int k = 0; k < 5; k++) {
+			// create inputs
+			int a = rn.nextInt(10) + 1;
+			int b = rn.nextInt(10) + 1;
+			// give examples of MaxInt
+			MaxInt(a, b);
+		}
+	}
+	
+	// MAXINT
+	public static int MaxInt(int a, int b) {
+		
+		// maxIntOutput
+		int maxIntOutput;
+		if (a > b) {
+			maxIntOutput = a;
+		} else {
+			maxIntOutput = b;
+		}
+		
+		// message to grader
+		String outputMessage = "The output of MaxInt(" + String.valueOf(a) + ", " + String.valueOf(b) +") is ";
+		System.out.print(outputMessage);
+		System.out.println(String.valueOf(maxIntOutput));
+		
+		// return
+		return maxIntOutput;
+	}
+
+}
