70769b956342cc36c7b659ea71a80987782dcf7c
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-14 19:18:00 +0100

    B1 done

---
+++ Assignment2/src/partB/B1.java
@@ -0,0 +1,28 @@
+package partB;
+
+public class B1 {
+	
+	// MAIN
+	public static void main(String[] args) {
+		for (int k = 1; k < 6; k++) {
+			Increment(k);
+		}
+	}
+	
+	// INCREMENT
+	public static int Increment(int a) {
+		
+		// incrementOutput
+		int incrementOutput = a + 1;
+		
+		// message to grader
+		String outputMessage = "The output of Increment(" + String.valueOf(a) + ") is ";
+		System.out.print(outputMessage);
+		System.out.println(String.valueOf(incrementOutput));
+		
+		// return value
+		return incrementOutput;
+	}
+	
+
+}
