90da6ac3407d65cdcbffb21b9b379a0355b3c16a
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-15 16:51:00 +0100

    B10 done

--- Assignment2/src/partB/B10.java
+++ Assignment2/src/partB/B10.java
@@ -25,6 +25,14 @@ public class B10 {
 	// LINEARSEARCH
 	public static int LinearSearch(int[] arrayA, int sizeN, int q) {
 		
+		System.out.println("B6: MinValueIndex");
+		System.out.println("");
+		
+		System.out.println("Note to Grader");
+		System.out.println("I kept int n as an input so that you could modify the size of the arrays");
+		System.out.println("All arrays are populated with random numbers between 1 and 10, inclusive");
+		System.out.println("");
+		
 		// random number rn
 		Random rn = new Random();
 		
@@ -33,22 +41,47 @@ public class B10 {
 			arrayA[k] = rn.nextInt(10) + 1;
 		}
 		
+		System.out.println("INPUTS");
+		System.out.println("arrayA is " + Arrays.toString(arrayA));
+		System.out.println("n is " + String.valueOf(sizeN));
+		System.out.println("q is " + String.valueOf(q));
+		System.out.println("");
+		
 		// index
+		System.out.println("Algorithm");
 		int index = 0;
+		System.out.println("index <-- 0");
+		System.out.println("");
 		while (index < sizeN && arrayA[index] != q) {
+			System.out.println(String.format("Check that index < n: %d < %d", index, sizeN));
+			System.out.println(String.format("Check that A[index] != q: %d != %d", arrayA[index], q));
 			index += 1;
+			System.out.println(String.format("index <-- index + 1 = %d + 1 = %d", index-1, index));
+			System.out.println("");
 		}
 		if (index == sizeN) { // comparing primitives, so == is fine
-			index = -1;
+			System.out.println("index == n, so return -1");
+			System.out.println("");
+			System.out.println("Summary");
+			String outputMessage = "LinearSearch(" + Arrays.toString(arrayA) + ", " + String.valueOf(sizeN)  + ", " + String.valueOf(q) + ") = -1";
+			System.out.print(outputMessage);
+			return -1;
+		} else {
+			System.out.println(String.format("Check that index < n: %d < %d", index, sizeN));
+			System.out.println(String.format("Find that A[index] = q: %d = %d", arrayA[index], q));
+			System.out.println("return index");
+			System.out.println("");
+			
+			// message to grader
+			System.out.println("Summary");
+			String outputMessage = "LinearSearch(" + Arrays.toString(arrayA) + ", " + String.valueOf(sizeN)  + ", " + String.valueOf(q) + ") is ";
+			System.out.print(outputMessage);
+			System.out.println(index);
+			
+			// return
+			return index;
 		}
 		
-		// message to grader
-		String outputMessage = "The output of LinearSearch(" + Arrays.toString(arrayA) + ", " + String.valueOf(sizeN)  + ", " + String.valueOf(q) + ") is ";
-		System.out.print(outputMessage);
-		System.out.println(index);
-		
-		// return
-		return index;
 	}
 
 }
