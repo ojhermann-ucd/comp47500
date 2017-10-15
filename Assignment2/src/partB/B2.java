2941627c02711d1c950bea1e0f8f9c0f885ce2ab
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-15 15:38:01 +0100

    B2 done

--- Assignment2/src/partB/B2.java
+++ Assignment2/src/partB/B2.java
@@ -12,6 +12,9 @@ public class B2 {
 		
 		System.out.println("B2: Average");
 		System.out.println("");
+		System.out.println("Here are five examples of Average(int a, int b)");
+		System.out.println("");
+		System.out.println("");
 		
 		// random number: rn
 		Random rn = new Random();
@@ -22,6 +25,8 @@ public class B2 {
 			int a = rn.nextInt(10) + 1;
 			int b = rn.nextInt(10) + 1;
 			// five examples of Average
+			System.out.println("Example: " + String.valueOf(k + 1));
+			System.out.println("--------------------------------------------------");
 			Average(a, b);
 		}
 		
@@ -30,13 +35,29 @@ public class B2 {
 	// AVERAGE
 	public static double Average(int a, int b) {
 		
+		// inputs
+		System.out.println("Input");
+		String inputMessage1 = "int a = " + String.valueOf(a);
+		String inputMessage2 = "int b = " + String.valueOf(b);
+		System.out.println(inputMessage1);
+		System.out.println(inputMessage2);
+		System.out.println("");
+		
 		//averageOutput
 		double averageOutput = ((double)a + (double)b) / 2.0;
+		System.out.println("Algorithm");
+		String outputMessage1 = "output = (a + b) / 2 = (" + String.valueOf(a) + " + " + String.valueOf(b) + ") / 2";
+		System.out.println(outputMessage1);
+		System.out.println("");
+		
 		
 		// message to grader
-		String outputMessage = "The output of Average(" + String.valueOf(a) + ", " + String.valueOf(b) +") is ";
+		System.out.println("Summary");
+		String outputMessage = "Average(" + String.valueOf(a) + ", " + String.valueOf(b) +") = ";
 		System.out.print(outputMessage);
 		System.out.println(String.valueOf(averageOutput));
+		System.out.println("");
+		System.out.println("");
 		
 		// return
 		return averageOutput;
