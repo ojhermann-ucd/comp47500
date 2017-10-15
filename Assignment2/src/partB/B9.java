96b3ffc0079653dd6105a427582d31beafd030ae
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-15 16:25:46 +0100

    B9 done

--- Assignment2/src/partB/B9.java
+++ Assignment2/src/partB/B9.java
@@ -12,12 +12,17 @@ public class B9 {
 		
 		System.out.println("B9: Power");
 		System.out.println("");
+		System.out.println("Here are five examples of Power(int a, int b) with random a, random b");
+		System.out.println("");
+		System.out.println("");
 		
 		// random number rn
 		Random rn = new Random();
 		
 		// five examples of Power
 		for (int k = 0; k < 5; k++) {
+			System.out.println("Example: " + String.valueOf(k + 1));
+			System.out.println("--------------------------------------------------");
 			// create inputs
 			int a = rn.nextInt(10) + 1;
 			int b = rn.nextInt(10) + 1;
@@ -29,16 +34,35 @@ public class B9 {
 	// POWER
 	public static long Power(long a, long b) {
 		
+		// input
+		System.out.println("Input");
+		String inputMessage1 = String.format("int a = %2d", a);
+		String inputMessage2 = String.format("int b = %2d", b);
+		System.out.println(inputMessage1);
+		System.out.println(inputMessage2);
+		System.out.println("");
+		
 		// power
 		long power = 1;
 		for (int k = 1; k < b + 1; k++) {
 			power *= a;
 		}
+		System.out.println("Algorithm");
+		power = 1;
+		System.out.println("power <-- 1");
+		for (int k = 1; k < b + 1; k++) {
+			System.out.println(String.format("power <-- power * a = %d * %d = %d", power, a, power * a));
+			power *= a;
+		}
+		System.out.println("");
 		
 		// message to grader
-		String outputMessage = "The output of Power(" + String.valueOf(a) + ", " + String.valueOf(b) +") is ";
+		System.out.println("Summary");
+		String outputMessage = "Power(" + String.valueOf(a) + ", " + String.valueOf(b) +") = ";
 		System.out.print(outputMessage);
 		System.out.println(String.valueOf(power));
+		System.out.println("");
+		System.out.println("");
 		
 		// return
 		return power;
