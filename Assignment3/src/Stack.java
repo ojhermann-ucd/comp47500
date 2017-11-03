b183c5776990566fe8b1f2607751980b301ec166
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-11-03 16:21:42 +0000

    Interface complete

---
+++ Assignment3/src/Stack.java
@@ -0,0 +1,24 @@
+// IMPORTS
+
+
+public interface Stack<E> {
+	
+	// size: returns the number of elements in the stack
+	public int size();
+	
+	// isEmpty: returns true if the stack is empty; false otherwise
+	public boolean isEmpty();
+	
+	// top: returns top element in the stack; throws exception if the stack is empty
+	public E top() throws EmptyStackException;
+	
+	// push: inserts an element to the top of the stack; throws an exception if the stack is full 
+	public void push(E element) throws FullStackException;
+	
+	// pop: removes the top element from the stack and returns it; throws an exception if the stack is empty
+	public E pop() throws EmptyStackException;
+	
+	// toString: returns a string representation of the stack, including the size and the contents, starting with the bottom
+	public String toString();
+	
+}
