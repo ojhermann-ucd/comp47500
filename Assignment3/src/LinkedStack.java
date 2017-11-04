0845d7e0ba34c974e38b141d77fe94d4ae90943b
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-11-04 20:29:16 +0000

    constructor done

--- Assignment3/src/LinkedStack.java
+++ Assignment3/src/LinkedStack.java
@@ -1,18 +1,16 @@
 
 public class LinkedStack<E> implements Stack<E> {
-	
+
 	
 	// NESTED CLASS: NODE
 	// ______________________________________________________________________
 	public class Node<E> {
-		
-		
+
 		// OBJECT FIELDS
 		// ______________________________________________________________________
 		private E element;
 		private Node<E> next;
-		
-		
+
 		// CONSTRUCTORS
 		// ______________________________________________________________________
 		// with argument
@@ -20,37 +18,55 @@ public class LinkedStack<E> implements Stack<E> {
 			this.element = element;
 			this.next = next;
 		}
-		
+
 		// no argument
 		public Node() {
 			this(null, null);
 		}
-		
-		
+
 		// ACCESSOR METHODS
 		// ______________________________________________________________________
 		// getElement()
 		public E getElement() {
 			return this.element;
 		}
-		
+
 		// getNext()
 		public Node<E> getNext() {
 			return this.next;
 		}
-		
-		
+
 		// SETTER METHODS
 		// ______________________________________________________________________
 		// setElement()
 		public void setElement(E element) {
 			this.element = element;
 		}
-		
+
 		// setNext
 		public void setNext(Node<E> next) {
 			this.next = next;
 		}
-		
+
 	}
+
+	
+	// OBJECT FIELDS
+	// ______________________________________________________________________
+	private Node<E> top;
+	private int size;
+	
+	
+	// CONSTRUCTORS
+	// ______________________________________________________________________
+	// no argument
+	public LinkedStack() {
+		this.top = null;
+		this.size = 0;
+	}
+	
+	
+	
+	
+	
 }
