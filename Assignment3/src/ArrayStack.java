006df27d5fb0d5bbecb5d9fa8b1e2151e1a1045c
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-11-04 12:29:57 +0000

    updating ArrayStack

--- Assignment3/src/ArrayStack.java
+++ Assignment3/src/ArrayStack.java
@@ -2,35 +2,44 @@
 import java.lang.reflect.Array;
 
 public class ArrayStack<E> implements Stack<E> {
+	
+	// CLASS FIELDS
+	public static final int CAPACITY = 1000;
 
 	// OBJECT FIELDS
 	// ______________________________________________________________________
-	private int arraySize;
-	private int arrayTop;
+	private int capacity;
+	private int top;
 	private E[] stack;
 	
 	// CONSTRUCTORS
 	//______________________________________________________________________
-	public ArrayStack(int arraySize) {
+	public ArrayStack(int capacity) {
 		// instatiate field values
-		this.arraySize = arraySize;
-		this.arrayTop = -1;
-		this.stack = (E[])new Object[this.arraySize];
+		this.capacity = capacity;
+		this.top = -1;
+		this.stack = (E[])new Object[this.capacity];
 	}
 	
 	public ArrayStack() {
-		this.arraySize = 1000;
-		this.arrayTop = -1;
-		this.stack = (E[])new Object[this.arraySize];
+		this(CAPACITY);
 	}
 	
 	// METHODS
+	//______________________________________________________________________
 	public int size() {
-		return this.stack.length;
+		// count objects until a null object is found or end of array is reached; return the count
+		int objectCount = 0;
+		while (objectCount < this.stack.length && this.stack[objectCount] != null) {
+			objectCount += 1;
+		}
+		return objectCount;
 	}
 	
 	public boolean isEmpty() {
 		return this.size() == 0;
 	}
+	
+	
 
 }
