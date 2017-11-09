54e91b5b2266ee94962ce44d4079aa62114e229d
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-11-09 16:15:57 +0000

    ArrayQueue v1 done

--- Assignment4/src/ArrayQueue.java
+++ Assignment4/src/ArrayQueue.java
@@ -34,11 +34,13 @@ public class ArrayQueue<E> implements Queue<E> {
 		if (this.isFull()) {
 			throw new FullQueueException();
 		} else {
-			this.rear = ++this.rear % this.capacity; // pre-increment the index and convert to an appropriate figure
+			this.rear = ++this.rear % this.capacity; // pre-increment the index
+														// and convert to an
+														// appropriate figure
 			this.queue[this.rear] = element; // put the element into the array
 		}
 	}
-	
+
 	// dequeue()
 	public E dequeue() throws EmptyQueueException {
 		if (this.isEmpty()) {
@@ -46,27 +48,51 @@ public class ArrayQueue<E> implements Queue<E> {
 		} else {
 			E returnE = this.queue[this.front]; // obtain the desired object
 			this.queue[this.front] = null; // clear out the existing entry
-			this.front = ++this.front % this.capacity; // pre-increment the index and convert to an appropriate figure
+			this.front = ++this.front % this.capacity; // pre-increment the
+														// index and convert to
+														// an appropriate figure
 			return returnE; // return the object
 		}
 	}
 
 	// SUPPORTING OPERATIONS
 	// ______________________________________________________
+	// size()
 	public int size() {
 		return (this.capacity + this.rear - this.front + 1) % this.capacity;
 	}
 
+	// isEmpty()
 	public boolean isEmpty() {
 		return this.rear == this.front;
 	}
-	
+
+	// isFull()
 	public boolean isFull() {
 		return this.size() == this.capacity;
 	}
 
-	public E front() throws EmptyQueueException;
+	// front()
+	public E front() throws EmptyQueueException {
+		if (this.isEmpty()) {
+			throw new EmptyQueueException();
+		} else {
+			return this.queue[this.front];
+		}
+	}
 
-	public String toString();
+	// toString()
+	public String toString() {
+		// create components of the return object
+		String stackString = "";
+		String indexString = "";
+		// populate return objects
+		for (int j = 0; j < this.capacity; j++) {
+			stackString += String.format("%-10s", this.queue[j].toString());
+			indexString += String.format("%-10d", j);
+		}
+		// return
+		return stackString + "\n" + indexString;
+	}
 
 }
