dc64919d849b6105f67bed00826abdfe2498b5b0
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-11-09 10:05:48 +0000

    starting Deque

---
+++ Assignment4/src/Deque.java
@@ -0,0 +1,34 @@
+
+public interface Deque<E> {
+	
+	// CORE OPERATIONS
+	// ______________________________________________________
+	
+	// inserts E at the start of the Deque
+	public void insertFirst(E element);
+	
+	// inserts E at the end of the Deque
+	public void insertLast(E element);
+	
+	// removes and returns the first element from the Deque 
+	public E removeFirst();
+	
+	// removes and returns the last element from the Deque
+	public E removeLast();
+	
+	// SUPPORT OPERATIONS
+	// ______________________________________________________
+	
+	// returns the number of elements in the Deque
+	public int size();
+	
+	// returns the empty-status of the Deque
+	public boolean isEmpty();
+	
+	// returns the first item of the Deque
+	public E front();
+	
+	// returns the last item of teh Deque
+	public E rear();
+
+}
